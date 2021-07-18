0e98282c796a194448f5b1fbd745e54234293f2a
Zachary-Roland <zsolo95@gmail.com>
2021-07-18 18:23:38 -0500

    adds pools and errors to mysql

---
+++ conf/mysql.conf.js
@@ -0,0 +1,29 @@
+const mysql = require("mysql");
+const util = require("util");
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABSE,
+  port: process.env.DB_PORT,
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error("Databse connection was closed.");
+    }
+    if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error("Database has too many connections.");
+    }
+    if (err.code === "ECONNREFUSED") {
+      console.error("Database connection was refused.");
+    }
+  }
+  if (connection) connection.release();
+  return;
+});
+
+const query = util.promisify(pool.query).bind(pool);
+module.exports = query;
