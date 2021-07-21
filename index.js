dd7e663b5bea5366de4aa0a5dae91d02372fb72a
Zachary-Roland <zsolo95@gmail.com>
2021-07-21 14:31:02 -0500

    adds build routes

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const garmsRoutes = require("./routes/garms.routes");
 const needsRoutes = require("./routes/needs.routes");
 
 app.use(express.json());
+app.use(express.static(__dirname + "/build"));
+
 app.use("/api/users", userRoutes);
 app.use("/api/garms", garmsRoutes);
 app.use("/api/needs", needsRoutes);
+
+app.get("*", (req, res) => {
+  return res.sendFile("/build/index.html", { root: __dirname + "/" });
+});
 
 app.listen(port, () => console.log(`It's alive! (on port ${port})`));
